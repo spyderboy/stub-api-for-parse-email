69f177fdb849d341973edf0df42e3bbbab1a29f6
Jose Antonio Licon <spyderboy@gmail.com>
2025-01-18 13:37:32 -0500

    Update index.js
    added some logging

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ app.use(express.json());
 // POST route to handle incoming data
 app.post('/submit', (req, res) => {
   const { title, description, url, senderEmail } = req.body;
+  console.log(`Recieved...`);
+  console.log(`title: ${title}`);
+  console.log(`description: ${description}`);
+  console.log(`url: ${url}`);
+  console.log(`senderEmail: ${senderEmail}`);
 
   // Basic validation
   if (!title || !description || !url || !senderEmail) {
     return res.status(400).json({ message: 'All fields are required.' });
   }
-  console.log(`Title: ${title}`);
-  console.log(`Title: ${description}`);
-  console.log(`Title: ${url}`);
-  console.log(`Title: ${senderEmail}`);
+  
   // For now, let's assume the process is successful
   res.status(200).json({ message: 'Success', data: { title, description, url, senderEmail } });
 });
